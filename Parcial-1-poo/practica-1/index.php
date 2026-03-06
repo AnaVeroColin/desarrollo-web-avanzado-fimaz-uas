bb324f1939dfdd7a5cac5485e9617ac9f4541a86
VeroColin <anavero.colin@gmail.com>
2026-03-05 22:54:54 -0700

    Cambios realizados

--- Parcial-1-poo/practica-1/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include 'Usuario.php';
-    $objUsuario = new Usuario('Ana Verónica Colín Lozano','anavero.colin@gmail.com');
-    echo '<p>Nombre: ' . $objUsuario->getNombre() . '</p>';
-    echo '<p>Correo: ' . $objUsuario->getCorreo() . '</p>';
-?>
-
-
