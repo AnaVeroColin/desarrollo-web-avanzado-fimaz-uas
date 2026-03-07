ee88e390e7f4539274e4a04e27d33bcd461cc6ef
VeroColin <anavero.colin@gmail.com>
2026-03-06 23:32:26 -0700

    Modificación include -> require_once

--- Parcial-1-poo/practica-1/index.php
+++ Parcial-1-poo/practica-1/index.php
@@ -1,5 +1,5 @@
 <?php
-    include 'Usuario.php';
+    require_once 'Usuario.php';
     $objUsuario = new Usuario('Ana Verónica Colín Lozano','anavero.colin@gmail.com');
     echo '<p>Nombre: ' . $objUsuario->getNombre() . '</p>';
     echo '<p>Correo: ' . $objUsuario->getCorreo() . '</p>';
