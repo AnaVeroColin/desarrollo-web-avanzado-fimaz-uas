a6e1fbd9fe6db71e90788926758801f80a11199f
VeroColin <anavero.colin@gmail.com>
2026-03-07 16:18:00 -0700

    Carpeta con clases

---
+++ Parcial-1-poo/practica-3/clases/Usuario.php
@@ -0,0 +1,35 @@
+<?php
+class Usuario
+    {
+    private $nombre;
+    private $correo;
+
+
+    function __construct($nombre, $correo)
+    {
+        $this->nombre=$nombre;
+        $this->setCorreo($correo);
+    }
+
+    function getNombre() {
+        return $this->nombre;
+    }
+
+    function getCorreo()
+    {
+        return $this->correo;
+    }
+
+    function setNombre($nombre) {
+        $this->nombre=$nombre;
+    }
+
+    function setCorreo($correo) {
+    if (filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+        $this->correo=$correo;
+        }
+        else {
+            throw new Exception("Correo Inválido");
+        } }
+}
+?>
