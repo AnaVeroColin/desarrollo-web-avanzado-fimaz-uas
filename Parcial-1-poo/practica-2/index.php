4524a462d502f29cee1e09b5d1e296df8a59d1a5
VeroColin <anavero.colin@gmail.com>
2026-03-08 00:57:24 -0700

    Se comento index.php

--- Parcial-1-poo/practica-2/index.php
+++ Parcial-1-poo/practica-2/index.php
@@ -1,9 +1,9 @@
 <?php
-    require_once 'Admin.php';
-    $objAdmin = new Admin('Ana Verónica Colín Lozano','anavero.colin@gmail.com');
-    echo '<p>Nombre: ' . $objAdmin->getNombre() . '</p>';
-    echo '<p>Correo: ' . $objAdmin->getCorreo() . '</p>';
-    echo '<p>Rol: ' . $objAdmin->getRol() . '</p>';
+    require_once 'Admin.php'; /*Se incluye el contenido del archivo Admin.php, permite el acceso a las clases, funciones o variables definidas en Usuario.php*/
+    $objAdmin = new Admin('Ana Verónica Colín Lozano','anavero.colin@gmail.com'); /*Se crea una nueva instancia/objeto para la clase Admin y se le proveen parámetros*/
+    echo '<p>Nombre: ' . $objAdmin->getNombre() . '</p>'; /*Mostrará el nombre de usuario almacenado */
+    echo '<p>Correo: ' . $objAdmin->getCorreo() . '</p>'; /*Mostrará el correo de usuario almacenado */
+    echo '<p>Rol: ' . $objAdmin->getRol() . '</p>'; /*Llama al método getRol y nos regresa el rola asociado con el usuario admin*/
 ?>
 
 
