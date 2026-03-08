aa24753e6a0329d9f9300be456894b4e5dd20122
VeroColin <anavero.colin@gmail.com>
2026-03-08 00:56:40 -0700

    Se comento usuario.php

--- Parcial-1-poo/practica-2/Usuario.php
+++ Parcial-1-poo/practica-2/Usuario.php
@@ -1,29 +1,29 @@
 <?php
 class Usuario
     {
-    private $nombre;
+    private $nombre; /*Se declaran propiedades priviadas de nombre y correo */
     private $correo;
 
 
-    function __construct($nombre, $correo)
+    function __construct($nombre, $correo)  /*Se inicializan las propiedades nombre y correo */
     {
         $this->nombre=$nombre;
         $this->correo=$correo;
     }
 
-    function getNombre() {
+    function getNombre() { /*La funcion getNombre() nos regresa el valor de la propiedad nombre del objeto*/
         return $this->nombre;
     }
 
-    function getCorreo()
+    function getCorreo() /*La función getCorreo() nos regresa el valor de la propiedad correo del objeto*/
     {
         return $this->correo;
     }
 
-    function setNombre($nombre) {
+    function setNombre($nombre) { /*Se establece el valor de la propiedad nombre*/
         $this->nombre=$nombre;
     }
-    function setCorreo($correo) {
+    function setCorreo($correo) { /*Se establece el valor de la propiedad correo*/
         $this->correo=$correo;
     }
 }
