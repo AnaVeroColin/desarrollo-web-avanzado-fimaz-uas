62e2b073c70e63ebd6aafaba417a5eed5acc5be0
VeroColin <anavero.colin@gmail.com>
2026-03-08 01:42:17 -0700

    Correccion en comentario

--- Parcial-1-poo/practica-2/index.php
+++ Parcial-1-poo/practica-2/index.php
@@ -1,5 +1,5 @@
 <?php
-    require_once 'Admin.php'; /*Se incluye el contenido del archivo Admin.php, permite el acceso a las clases, funciones o variables definidas en Usuario.php*/
+    require_once 'Admin.php'; /*Se incluye el contenido del archivo Admin.php, permite el acceso a las clases, funciones o variables definidas*/
     $objAdmin = new Admin('Ana Verónica Colín Lozano','anavero.colin@gmail.com'); /*Se crea una nueva instancia/objeto para la clase Admin y se le proveen parámetros*/
     echo '<p>Nombre: ' . $objAdmin->getNombre() . '</p>'; /*Mostrará el nombre de usuario almacenado */
     echo '<p>Correo: ' . $objAdmin->getCorreo() . '</p>'; /*Mostrará el correo de usuario almacenado */
