bd07731fb2ee6d339ba47aa3239eebd1509e302d
VeroColin <anavero.colin@gmail.com>
2026-03-08 00:57:03 -0700

    Se comento Admin.php

--- Parcial-1-poo/practica-2/Admin.php
+++ Parcial-1-poo/practica-2/Admin.php
@@ -1,7 +1,7 @@
 <?php
-require_once 'Usuario.php';
-    class Admin extends Usuario {
-        public function getRol() {
+require_once 'Usuario.php'; /*Se le permite a la clase Admin tener acceso al código de Usuario.php*/
+    class Admin extends Usuario { /*La clase Admin se hace hija de la clase Usuario*/
+        public function getRol() { /*Función getRol nos regresa el rol de Administrador*/
             return "Administrador";
         }
     }
