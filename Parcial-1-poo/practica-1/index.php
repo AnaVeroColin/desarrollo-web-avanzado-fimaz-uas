e97ec63206fc3d431486d55d9ce0ab5063dcbb40
VeroColin <anavero.colin@gmail.com>
2026-03-08 00:48:37 -0700

    Código comentado

--- Parcial-1-poo/practica-1/index.php
+++ Parcial-1-poo/practica-1/index.php
@@ -1,8 +1,7 @@
 <?php
-    require_once 'Usuario.php';
-    $objUsuario = new Usuario('Ana Verónica Colín Lozano','anavero.colin@gmail.com');
-    echo '<p>Nombre: ' . $objUsuario->getNombre() . '</p>';
-    echo '<p>Correo: ' . $objUsuario->getCorreo() . '</p>';
+    require_once 'Usuario.php'; /*Se incluye el contenido del archivo Usuario.php, permite el acceso a las clases, funciones o variables definidas en Usuario.php*/
+    $objUsuario = new Usuario('Ana Verónica Colín Lozano','anavero.colin@gmail.com'); /*Se crea una nueva instancia/objeto para la clase Usuario y se le proveen parámetros*/
+    echo '<p>Nombre: ' . $objUsuario->getNombre() . '</p>'; /*Mostrará el nombre de usuario almacenado */
+    echo '<p>Correo: ' . $objUsuario->getCorreo() . '</p>'; /*Mostrará el correo almacenado  */
 ?>
-
 
