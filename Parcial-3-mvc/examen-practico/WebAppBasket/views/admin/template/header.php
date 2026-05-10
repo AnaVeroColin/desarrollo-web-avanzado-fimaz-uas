0d690a7505661f49d0e1a861e64e27c42ced9843
VeroColin <anavero.colin@gmail.com>
2026-05-10 16:56:12 -0700

    Templates para header y footer

---
+++ Parcial-3-mvc/examen-practico/WebAppBasket/views/admin/template/header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<!--Ana Verónica Colín Lozano LISI 3-1-->
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Web App Basketball</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container"></div>
