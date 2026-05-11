f77f1c10db8211821f084f1e587c9bca19281a33
VeroColin <anavero.colin@gmail.com>
2026-05-10 17:04:49 -0700

    Archivo delete hasta penultimo video

---
+++ Parcial-3-mvc/examen-practico/WebAppBasket/views/admin/deleteTorneo.php
@@ -0,0 +1,11 @@
+<?php
+    //Ana Verónica Colín Lozano LISI 3-1
+    require_once("../../controllers/torneosController.php");
+
+    $objController = new torneosController();
+
+    //Obtener el ID desde el botón que mandará eliminar el registro
+    //Lo obtendremos de la pantalla del listado general de torneos
+
+    $objController->delete($_GET['id']);
+?>
